b8430ce82f6805a763dd750d658ba977e3f5d9c1
whitney <school07whitney11@gmail.com>
2017-10-16 19:00:22 +0000

    security update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
 
+  get 'my_pages/home'
+
+  get 'my_pages/help'
+
+  get 'my_pages/about'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
- # root 'application#hello'
+ # root 'users#index'
   
   root 'users#index'
   
